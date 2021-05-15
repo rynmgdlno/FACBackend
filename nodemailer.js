729f4d39daa96711a4db253099a493de24984299
rynmgdlno <rynmgdlno@gmail.com>
2021-05-14 23:39:15 -0700

    nodemailer setup

---
+++ nodemailer.js
@@ -0,0 +1,28 @@
+import nodemailer from 'nodemailer'
+
+async function main() {
+  let testAccount = await nodemailer.createTestAccount();
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+    auth: {
+      user: testAccount.user,
+      pass: testAccount.pass,
+    },
+  })
+
+  let info = await transporter.sendMail({
+    from: '"Fred Foo" <foo@example.com>',
+    to: "bar@example.com",
+    subject: "Hello",
+    text: "Hello world?",
+    html: "<b>Hello world with HTML?</b>"
+  })
+
+  console.log("message sent: %s", info.messageId)
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info))
+}
+
+main().catch(console.error)
