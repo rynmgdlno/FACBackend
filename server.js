cced1f849a7921f3aa4f70608ebbc45b68f494dc
rynmgdlno <53541580+rynmgdlno@users.noreply.github.com>
2021-05-19 16:06:10 -0700

    initializing server

--- server.js
+++ server.js
@@ -2,19 +2,21 @@
 
 import express from 'express'
 import cors from 'cors'
+import nodemailer from 'nodemailer'
 
-// Constants
-const PORT = 42069
-const HOST = '0.0.0.0'
+const PORT = process.env.PORT || 5000
+const HOST = process.env.HOST || '0.0.0.0'
 
-// App
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
 
 app.listen(PORT, HOST)
 console.log(`Server is running on http://${HOST}:${PORT}`)
+
+// Routes //
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
