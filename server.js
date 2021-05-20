f3ad35cffd00f0ee7d76d013699b2a87e10783cd
rynmgdlno <53541580+rynmgdlno@users.noreply.github.com>
2021-05-19 21:35:14 -0700

    nodemailer now running (test only)

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 import express from 'express'
 import cors from 'cors'
 import nodemailer from 'nodemailer'
-import {auth} from './email-credentials.js'
+import { auth } from './email-credentials.js'
 
 const PORT = process.env.PORT || 5000
 const HOST = process.env.HOST || '0.0.0.0'
@@ -25,44 +25,39 @@ let transporter = nodemailer.createTransport({
   }
 })
 
-
 // Routes //
-app.get('/', function (req, res) {
-  res.send('hello world')
-})
 
-app.post('/mail', (req, res, next) => {
+app.post('/mail', (req, res) => {
+  let name = req.body.name
   let email = req.body.email
   let message = req.body.message
   let subject = req.body.subject
-  let name = req.body.name
 
-  async function main() {
-    const mailOptions = {
-      from: name,
-      to: email,
-      subject: subject,
-      html: `Sender Name: ${name} <br /> Sender Email: ${email} <br /><br /> Message Content: <br />${message}`
-    }
-    transporter.sendMail(mailOptions, (err, data) => {
-      if (err) {
-        res.json({
-          status: err
-        })
-      } else {
-        res.json({
-          status: "success"
-        })
-        console.log("Email Sent" + data.response)
-      }
-    })
-    transporter.verify(function(err, success) {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log("Server is good to go")
-      }
-    })
+  const mailParams = {
+    from: name,
+    to: 'ryannmagdaleno@icloud.com',
+    subject: subject,
+    html: `Sender Name: ${name} <br /> Sender Email: ${email} <br /><br /> Message Content: <br />${message}`
   }
-  main().catch(console.error)
+
+  transporter.sendMail(mailParams, (err, data) => {
+    if (err) {
+      res.json({
+        status: err
+      })
+    } else {
+      res.json({
+        status: "success"
+      })
+      console.log("Email Sent " + data.response)
+    }
+  })
+
+  transporter.verify(function (err, success) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(success, req.body)
+    }
+  })
 })
