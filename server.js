761a728e426b95943c06f1170b677f08dc903b08
rynmgdlno <53541580+rynmgdlno@users.noreply.github.com>
2021-05-20 11:41:35 -0700

    nodemailer now running (test only)

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 import express from 'express'
 import cors from 'cors'
 import nodemailer from 'nodemailer'
+
+// import { mailer } from './nodemailer.js'
 import { auth } from './email-credentials.js'
 
 const PORT = process.env.PORT || 5000
@@ -17,25 +19,27 @@ app.use(cors())
 app.listen(PORT, HOST)
 console.log(`Server is running on http://${HOST}:${PORT}`)
 
-let transporter = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: auth.user,
-    pass: auth.pass
-  }
-})
-
-// Routes //
 
 app.post('/mail', (req, res) => {
   let name = req.body.name
   let email = req.body.email
   let message = req.body.message
   let subject = req.body.subject
+  let recipient = req.body.recipient
+  
+  let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: auth.user,
+      pass: auth.pass
+    }
+  })
+  
+  // Routes
 
   const mailParams = {
     from: name,
-    to: 'ryannmagdaleno@icloud.com',
+    to: recipient,
     subject: subject,
     html: `Sender Name: ${name} <br /> Sender Email: ${email} <br /><br /> Message Content: <br />${message}`
   }
@@ -60,4 +64,6 @@ app.post('/mail', (req, res) => {
       console.log(success, req.body)
     }
   })
+
+  // mailer().catch(console.error)
 })
